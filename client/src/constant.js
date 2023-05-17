095b44e19fd06680a32d1014beda5b1cdda266bc
Zeyad-Sayed-70 <102079450+Zeyad-Sayed-70@users.noreply.github.com>
2023-05-17 23:11:52 +0300

    make life better

--- client/src/constant.js
+++ client/src/constant.js
@@ -1,3 +1,3 @@
-// export const BACK_END_URL = 'https://phonix2022.herokuapp.com'
-export const BACK_END_URL = 'http://localhost:5000'
+export const BACK_END_URL = 'https://phanox-server-80m1.onrender.com/'
+// export const BACK_END_URL = 'http://localhost:5000'
 export const VERSION = '1.2.0'
